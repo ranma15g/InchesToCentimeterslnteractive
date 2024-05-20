2c44a083bda0fecf3b03662ba86202f12996e12a
ranma15g <ranma15g@gmail.com>
2024-05-20 01:54:57 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,63 @@
+using System;
+
+/*
+
+2-1.1.  Convert the InchesToCentimeters program to an interactive application named InchesToCentimeterslnteractive.
+
+ Instead of assigning a value to the inches variable, accept the value from the user as input.
+
+*/
+
+class Program
+{
+
+      static void Main(string[] args)
+      {
+
+        /* Here is the interactive portion of the assignment where we are
+           checking to see if our user enters a whole number for the inches.
+           Whole number because of the parameters of the last assignment had
+           us call for an int and that has not changed in the instructions.
+           We use a Console.WriteLine() to receive data from the user. */
+          Console.WriteLine("Please enter a number with no decimals:");
+          var inches = Console.ReadLine(); /* Here is where we assign a value
+                                              from user input. */
+
+        /* Here is where we declare a named constant 
+        that holds the number of centimeters in 
+        an inch: 2.54. */
+          const double CENTIMETERS_PER_INCH = 2.54;
+
+                
+          /* We created the int number to be used as a test 
+             on the parse to ensure the user entered a whole 
+             number */
+          int number;
+        
+          /* if we did follow instruction and use an integer to be the thing
+              we test against for our test Units */
+          if (int.TryParse(inches, out number))
+          {
+
+            /* Here is where we convert inches to centimeters */
+            double centimeters = number * CENTIMETERS_PER_INCH;
+              /* we implicitly type cased number which is an integer and not
+                 inches because that was recorded as a string. */
+
+            /* Our reward for entering the correct data is a message calculating
+               inches to centimeter */
+            Console.WriteLine("{0} inches is centimeters = {1}.", inches, centimeters);           
+    
+          }
+  
+        
+        
+          else // We did not enter a whole number        
+          {
+            // We tell the user to go pound sand
+            Console.WriteLine("I said a number with no decimals, Good Day Sir.");
+          }
+
+      }    
+      
+}
